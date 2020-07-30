94b596118b04d2057144539da05d34ea7c93a585
unknown <2512281468@qq.com>
2020-07-30 20:25:49 +0800

    frist

---
+++ src/components/list/list.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+</div>
+</template>
+
+<script>
+export default {
+ data () {
+  return {
+  };
+ },
+
+ mounted(){},
+ methods: {},
+ components: {},
+ computed: {},
+
+
+
+}
+
+</script>
+<style scoped>
+</style>
